ec122e4150468a693ff004440c3bbabee32cf296
Brian Levinstein <blevinstein@gmail.com>
2015-01-04 00:22:29 -0800

    Pretty rendering.

--- chess.rkt
+++ chess.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; piece data
+
 (define piece-values
   (hash
     'P 1
@@ -9,8 +11,30 @@
     'Q 9
     'K 0))
 
+(define piece-codes
+  (hash
+    'K "\u265a"
+    'Q "\u265b"
+    'R "\u265c"
+    'B "\u265d"
+    'N "\u265e"
+    'P "\u265f"))
+
+(define file-string
+  (hash
+    0 "a"
+    1 "b"
+    2 "c"
+    3 "d"
+    4 "e"
+    5 "f"
+    6 "g"
+    7 "h"))
+
 (define back-row
   '(R N B Q K B N R))
+
+; terminal escape sequences
 
 (define (esc . codes)
   (string-append
@@ -18,11 +42,13 @@
     (string-join (map number->string codes) ";")
     "m"))
 
-(define bg-white (esc 48 5 246)) ; light gray
-(define bg-black (esc 48 5 242)) ; dark gray
+(define bg-white (esc 48 5 246))
+(define bg-black (esc 48 5 242))
 (define fg-white (esc 97))
 (define fg-black (esc 30))
 (define reset (esc 0))
+
+; definition of a new board
 
 (define (new-grid)
   (append
@@ -32,22 +58,34 @@
     (list (build-list 8 (λ (i) (cons 'white 'P))))
     (list (map (λ (piece) (cons 'white piece)) back-row))))
 
-(define (render-square background square)
+; print subroutines
+
+(define (print-square background square)
   (display (if (equal? background 'black) bg-black bg-white))
   (display " ")
   (match square
     [(cons player piece)
       (display (if (equal? player 'black) fg-black fg-white))
-      (display (symbol->string piece))]
+      (display (hash-ref piece-codes piece))]
     [null (display " ")])
   (display " "))
 
 (define (print-grid grid)
+  (display "  ")
+  (for ([j (in-range 8)])
+    (display " ")
+    (display j)
+    (display " "))
+  (displayln "")
   (for ([i (in-range 8)])
+    (display (hash-ref file-string i))
+    (display " ")
     (for ([j (in-range 8)])
       (define square (list-ref (list-ref grid i) j))
-      (render-square (if (equal? (modulo (+ i j) 2) 0) 'black 'white) square))
+      (print-square (if (equal? (modulo (+ i j) 2) 0) 'black 'white) square))
     (displayln reset)))
+
+; grid->position conversion code
 
 (define (grid-player-position grid player)
   (filter (compose1 not null?)
