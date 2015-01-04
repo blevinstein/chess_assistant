7fb285f34e28004794fc1b689c9abc63fb71a4df
Brian Levinstein <blevinstein@gmail.com>
2015-01-04 00:04:27 -0800

    Improved rendering.

--- chess.rkt
+++ chess.rkt
@@ -18,8 +18,8 @@
     (string-join (map number->string codes) ";")
     "m"))
 
-(define bg-white (esc 47)) ; light gray
-(define bg-black (esc 100)) ; dark gray
+(define bg-white (esc 48 5 246)) ; light gray
+(define bg-black (esc 48 5 242)) ; dark gray
 (define fg-white (esc 97))
 (define fg-black (esc 30))
 (define reset (esc 0))
@@ -32,16 +32,21 @@
     (list (build-list 8 (λ (i) (cons 'white 'P))))
     (list (map (λ (piece) (cons 'white piece)) back-row))))
 
+(define (render-square background square)
+  (display (if (equal? background 'black) bg-black bg-white))
+  (display " ")
+  (match square
+    [(cons player piece)
+      (display (if (equal? player 'black) fg-black fg-white))
+      (display (symbol->string piece))]
+    [null (display " ")])
+  (display " "))
+
 (define (print-grid grid)
-  (for ([row grid] [i (in-range 8)])
-    (for ([square row] [j (in-range 8)])
-      ; set background color
-      (display (if (equal? (modulo (+ i j) 2) 0) bg-black bg-white))
-      (match square
-        [(cons player piece)
-          (display (if (equal? player 'black) fg-black fg-white))
-          (display (symbol->string piece))]
-        [null (display " ")]))
+  (for ([i (in-range 8)])
+    (for ([j (in-range 8)])
+      (define square (list-ref (list-ref grid i) j))
+      (render-square (if (equal? (modulo (+ i j) 2) 0) 'black 'white) square))
     (displayln reset)))
 
 (define (grid-player-position grid player)
