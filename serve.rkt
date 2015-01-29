b5283e3306466216144436e167aecdabce927660
Brian Levinstein <blevinstein@gmail.com>
2015-01-29 02:08:12 -0800

    Add server restart on filesystem changes.

--- serve.rkt
+++ serve.rkt
@@ -1,14 +1,37 @@
 #lang racket
 
+; This file will run the servlet, restarting whenever a file system change is
+; detected in the current directory.
+
+(require racket/rerequire)
 (require web-server/servlet-env)
 
-(require "servlet.rkt")
+(dynamic-rerequire (string->path "servlet.rkt"))
+(define start (dynamic-require "servlet.rkt" 'start))
 
-(define (main)
+(define (run-server)
   (serve/servlet start
     #:launch-browser? #f
     #:port 8000
     #:server-root-path (current-directory)
     #:servlet-regexp #rx""))
+
+(define (main)
+  (let loop ()
+    ; create a filesystem change event
+    (define change-watcher (filesystem-change-evt (current-directory)))
+    ; run the server
+    (define cust (make-custodian))
+    (parameterize ([current-custodian cust])
+      (thread run-server))
+    ; wait until a filesystem change event is received
+    (sync change-watcher)
+    ; shutdown server
+    (custodian-shutdown-all cust)
+    ; reload servlet.rkt if necessary
+    (dynamic-rerequire "servlet.rkt")
+    (set! start (dynamic-require "servlet.rkt" 'start))
+    (displayln "Restarting server...")
+    (loop)))
 
 (main)
