8f733a9cc819984b162d4694c58be16ec167732e
Brian Levinstein <blevinstein@gmail.com>
2016-02-01 23:33:53 -0800

    Add RiderMove.all

--- src/scala/com/blevinstein/chess/Move.scala
+++ src/scala/com/blevinstein/chess/Move.scala
@@ -1,29 +1,5 @@
 package com.blevinstein.chess
 
-object Move {
-  def allTransformations(offset: (Int, Int)): List[(Int, Int)] = offset match {
-    case (f, r) => Set(
-        (f, r), (-f, r), (f, -r), (-f, -r),
-        (r, f), (-r, f), (r, -f), (-r, -f)).toList
-  }
-  def tryMove(
-      position: Position,
-      toMove: Color,
-      source: Location,
-      dest: Location): Option[Position] = {
-    (position(source), position(dest)) match {
-      case (Some((sourceColor, piece)), None)
-          if sourceColor == toMove =>
-          Some(position + (source, None) + (dest, Some((sourceColor, piece))))
-      case (Some((sourceColor, piece)), Some((destColor, _)))
-          if sourceColor == toMove && destColor == !toMove =>
-          // Capture
-          Some(position + (source, None) + (dest, Some((toMove, piece))))
-      case _ => None
-    }
-  }
-}
-
 trait Move {
   // NOTE: To fully embed information about valid moves, we need to have access
   // to the entire history of the board. Consider en passant and castling.
@@ -31,11 +7,43 @@ trait Move {
       Option[Position]
 }
 
+object Move {
+  // Helper methods
+
+  def allTransformations(offset: (Int, Int)): List[(Int, Int)] = offset match {
+    case (f, r) => Set(
+        (f, r), (-f, r), (f, -r), (-f, -r),
+        (r, f), (-r, f), (r, -f), (-r, -f)).toList
+  }
+
+  def mul(offset: (Int, Int), k: Int): (Int, Int) =
+      (offset._1 * k, offset._2 * k)
+
+  def tryMove(
+      position: Position,
+      toMove: Color,
+      source: Location,
+      dest: Location): Option[Position] = {
+    (position(source), position(dest)) match {
+      // Move to open space
+      case (Some((sourceColor, piece)), None)
+          if sourceColor == toMove =>
+          Some(position + (source, None) + (dest, Some((sourceColor, piece))))
+      // Capture
+      case (Some((sourceColor, piece)), Some((destColor, _)))
+          if sourceColor == toMove && destColor == !toMove =>
+          Some(position + (source, None) + (dest, Some((toMove, piece))))
+      // Can't move
+      case _ => None
+    }
+  }
+}
+
 object LeaperMove {
-  def all(source: Location, offset: (Int, Int)) =
+  def all(source: Location, offset: (Int, Int)): List[LeaperMove] =
       Move.allTransformations(offset).
-          filter(o => (source + o).isValid). // bounds check
-          map{LeaperMove(source, _)}.
+          filter(offset => (source + offset).isValid). // bounds check
+          map(offset => LeaperMove(source, offset)).
           toList
 }
 case class LeaperMove(source: Location, offset: (Int, Int)) extends Move {
@@ -44,20 +52,31 @@ case class LeaperMove(source: Location, offset: (Int, Int)) extends Move {
       Move.tryMove(position, toMove, source, source + offset)
 }
 
+object RiderMove {
+  def all(source: Location, offset: (Int, Int)): List[RiderMove] =
+      Move.allTransformations(offset).
+          map(offset =>
+              (1 to 8).map(dist => (offset, dist))
+          ).flatten.
+          filter{ case (offset, dist) =>
+              (source + Move.mul(offset, dist)).isValid
+          }.
+          map{ case (offset, dist) => RiderMove(source, offset, dist) }.
+          toList
+}
 case class RiderMove(source: Location, offset: (Int, Int), dist: Int) extends
     Move {
-  private def mul(offset: (Int, Int), k: Int): (Int, Int) =
-      (offset._1 * k, offset._2 * k)
 
   def apply(history: History, position: Position, toMove: Color):
       Option[Position] = {
     val emptyBetween = (1 until dist).
-        forall(i => position(source + mul(offset, i + 1)) == None)
+        forall(i => position(source + Move.mul(offset, i + 1)) == None)
 
     if (emptyBetween) {
-      Move.tryMove(position, toMove, source, source + mul(offset, dist))
+      Move.tryMove(position, toMove, source, source + Move.mul(offset, dist))
     } else {
       None
     }
   }
 }
+
