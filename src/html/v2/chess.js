0b2f4c32d09a29ce27dc2e3a868349b41d81de3a
Brian Levinstein <blevinstein@gmail.com>
2016-03-10 23:17:34 -0800

    Build basic ChessBoard UI.

--- src/html/v2/chess.js
+++ src/html/v2/chess.js
@@ -1,12 +1,17 @@
-var border = {
+var boardStyle = {
   "border": "5px solid black"
 };
 
-var centerText = {
+var textStyle = {
   "alignmentBaseline": "central",
   "fontFamily": "sans-serif",
   "pointerEvents": "none",
   "textAnchor": "middle"
+};
+
+var rowStyle = {
+  "display": "flex",
+  "flexDirection": "row",
 };
 
 function getCharacter(color, piece) {
@@ -25,14 +30,75 @@ function getCharacter(color, piece) {
   else return "";
 }
 
+Number.prototype.upto = function(t) {
+  var list = [];
+  for (var i = this; i < t; i++) list.push(i);
+  return list;
+};
+
+Number.prototype.downto = function(t) {
+  var list = [];
+  for (var i = this; i > t; i++) list.push(i);
+  return list;
+};
+
+/* Given [color] "white"/"black", returns CSS color for rendering */
+function getCssColor(color) {
+  if (color == "white") return "#ddd";
+  if (color == "black") return "#666";
+  else return "purple";
+}
+
+function allLocations() {
+  return (0).upto(8).map(function(rank) {
+    var r = String.fromCharCode(49 + rank);
+    return (0).upto(8).map(function(file) {
+      var f = String.fromCharCode(97 + file);
+      return f + r;
+    });
+  }).reverse();
+}
+
+function fileOf(loc) { return loc.charCodeAt(0) - 97; }
+
+function rankOf(loc) { return loc.charCodeAt(1) - 49; }
+
+function getBackground(loc) {
+  return (fileOf(loc) + rankOf(loc)) % 2 ? "white" : "black";
+}
+
 window.ChessSquare = React.createClass({
   render() {
     return (
-      <svg style={border} width="100" height="100">
-        <text style={centerText} x="50" y="50" fontSize="50">
-          {getCharacter(this.props.color, this.props.piece)}
-        </text>
+      <svg width="100" height="100">
+        <rect width="100" height="100" fill={getCssColor(this.props.background)}></rect>
+        {this.props.piece != undefined
+            ? <text style={textStyle} x="50" y="50" fontSize="50">
+                {getCharacter(this.props.color, this.props.piece)}
+            </text>
+            : ""}
       </svg>
+    );
+  }
+});
+
+window.ChessBoard = React.createClass({
+  render() {
+    return (
+      <div style={boardStyle}>
+        {allLocations().map(function (row) {
+          return (
+            <div style={rowStyle}>
+              {row.map(function (loc) {
+                return <ChessSquare
+                    color={getBackground(loc)}
+                    piece="P"
+                    background={getBackground(loc)} />;
+              })}
+            </div>
+          )
+        })}
+      </div>
     );
   }
 });
